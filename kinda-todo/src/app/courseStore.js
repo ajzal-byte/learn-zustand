4e706e04dfffe06c3fb4d5d7a8500e53c6ad3ef1
ajzal-byte <ajzalaju10@gmail.com>
2024-04-01 17:56:35 +0530

    new course at bottom

--- kinda-todo/src/app/courseStore.js
+++ kinda-todo/src/app/courseStore.js
@@ -6,7 +6,7 @@ const courseStore = (set) => ({
   courses: [],
   addCourse: (course) => {
     set((state) => ({
-      courses: [course, ...state.courses],
+      courses: [...state.courses, course],
     }));
   },
   removeCourse: (courseId) => {
@@ -16,9 +16,13 @@ const courseStore = (set) => ({
   },
   toggleCourseStatus: (courseId) => {
     set((state) => ({
-      courses: state.courses.map(course => course.id === courseId ? {...course, completed: !course.completed} : course)
-    }))
-  }
+      courses: state.courses.map((course) =>
+        course.id === courseId
+          ? { ...course, completed: !course.completed }
+          : course
+      ),
+    }));
+  },
 });
 
 const useCourseStore = create(
